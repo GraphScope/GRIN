7bbd07e3f39f896a6837b4642e66ed23cc331f05
diwen.zdw <diwen.zdw@alibaba-inc.com>
2023-03-23 20:10:14 +0800

    quick fix

--- property/topology.h
+++ property/topology.h
@@ -29,11 +29,11 @@ size_t grin_get_edge_num_by_type(GRIN_GRAPH, GRIN_DIRECTION, GRIN_EDGE_TYPE);
 #endif
 
 #if defined(GRIN_ENABLE_GRAPH_PARTITION) && defined(GRIN_WITH_VERTEX_PROPERTY)
-size_t grin_get_total_vertex_num_by_type(GRIN_PARTITIONED_GRAPH);
+size_t grin_get_total_vertex_num_by_type(GRIN_PARTITIONED_GRAPH, GRIN_VERTEX_TYPE);
 #endif
 
 #if defined(GRIN_ENABLE_GRAPH_PARTITION) && defined(GRIN_WITH_EDGE_PROPERTY)
-size_t grin_get_total_edge_num_by_type(GRIN_PARTITIONED_GRAPH);
+size_t grin_get_total_edge_num_by_type(GRIN_PARTITIONED_GRAPH, GRIN_EDGE_TYPE);
 #endif
 
 #ifdef GRIN_ASSUME_BY_TYPE_VERTEX_ORIGINAL_ID
