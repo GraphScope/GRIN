49798f3aea07a53ff14b48ce02fdf4e6743249e9
diwen.zdw <diwen.zdw@alibaba-inc.com>
2023-04-13 20:09:13 +0800

    fix proto and define grin msg api

---
+++ proto/message.h
@@ -0,0 +1,23 @@
+/** Copyright 2020 Alibaba Group Holding Limited.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ @file message.h
+ @brief Define storage feature protobuf message
+*/
+
+#ifndef GRIN_INCLUDE_PROTO_MESSAGE_H_
+#define GRIN_INCLUDE_PROTO_MESSAGE_H_
+
+const char* grin_get_static_storage_feature_msg();
+
+#endif // GRIN_INCLUDE_PROTO_MESSAGE_H_
