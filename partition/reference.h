2c7e7c50a5726bff97bea54169117ef28cf5e96e
有理 <youli.lx@alibaba-inc.com>
2023-05-11 11:39:03 +0800

    Fix macros in reference.h

--- partition/reference.h
+++ partition/reference.h
@@ -69,7 +69,7 @@ GRIN_VERTEX_REF grin_deserialize_int64_to_vertex_ref(GRIN_GRAPH, long long int);
 GRIN_PARTITION_LIST grin_get_master_vertex_mirror_partition_list(GRIN_GRAPH, GRIN_VERTEX);
 #endif
 
-#ifdef GRIN_TRAIT_MASTER_VERTEX_MIRROR_PARTITION_LIST
+#ifdef GRIN_TRAIT_MIRROR_VERTEX_MIRROR_PARTITION_LIST
 GRIN_PARTITION_LIST grin_get_mirror_vertex_mirror_partition_list(GRIN_GRAPH, GRIN_VERTEX);
 #endif
 
@@ -97,7 +97,7 @@ bool grin_is_mirror_edge(GRIN_GRAPH, GRIN_EDGE);
 GRIN_PARTITION_LIST grin_get_master_edge_mirror_partition_list(GRIN_GRAPH, GRIN_EDGE);
 #endif
 
-#ifdef GRIN_TRAIT_MASTER_EDGE_MIRROR_PARTITION_LIST
+#ifdef GRIN_TRAIT_MIRROR_EDGE_MIRROR_PARTITION_LIST
 GRIN_PARTITION_LIST grin_get_mirror_edge_mirror_partition_list(GRIN_GRAPH, GRIN_EDGE);
 #endif
 
