748efde44b0bb1af0551478afea8c96fcc28c2d6
diwen.zdw <diwen.zdw@alibaba-inc.com>
2023-06-06 14:19:08 +0800

    update pk APIs and add grin extension

---
+++ include/index/pk.h
@@ -0,0 +1,45 @@
+/** Copyright 2020 Alibaba Group Holding Limited.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ @file order.h
+ @brief Define the vertex ordering predicate APIs
+*/
+
+#ifndef GRIN_INCLUDE_INDEX_PK_H_
+#define GRIN_INCLUDE_INDEX_PK_H_
+
+#ifdef GRIN_ENABLE_VERTEX_PK_INDEX
+GRIN_VERTEX grin_get_vertex_by_primary_keys_row(GRIN_GRAPH, GRIN_VERTEX_TYPE, GRIN_ROW);
+#endif
+
+#if defined(GRIN_ENABLE_VERTEX_PK_INDEX) && defined(GRIN_ENABLE_VERTEX_PK_OF_INT64)
+GRIN_VERTEX grin_get_vertex_by_pk_of_int64(GRIN_GRAPH, GRIN_VERTEX_TYPE, long long int);
+#endif
+
+#if defined(GRIN_ENABLE_VERTEX_PK_INDEX) && defined(GRIN_ENABLE_VERTEX_PK_OF_STRING)
+GRIN_VERTEX grin_get_vertex_by_pk_of_string(GRIN_GRAPH, GRIN_VERTEX_TYPE, const char*);
+#endif
+
+#ifdef GRIN_ENABLE_EDGE_PK_INDEX
+GRIN_EDGE grin_get_edge_by_primary_keys_row(GRIN_GRAPH, GRIN_EDGE_TYPE, GRIN_ROW);
+#endif
+
+#if defined(GRIN_ENABLE_EDGE_PK_INDEX) && defined(GRIN_ENABLE_EDGE_PK_OF_INT64)
+GRIN_EDGE grin_get_edge_by_pk_of_int64(GRIN_GRAPH, GRIN_EDGE_TYPE, long long int);
+#endif
+
+#if defined(GRIN_ENABLE_EDGE_PK_INDEX) && defined(GRIN_ENABLE_EDGE_PK_OF_STRING)
+GRIN_EDGE grin_get_edge_by_pk_of_string(GRIN_GRAPH, GRIN_EDGE_TYPE, const char*);
+#endif
+
+#endif // GRIN_INCLUDE_INDEX_PK_H_
--- include/property/primarykey.h
+++ include/property/primarykey.h
@@ -24,49 +24,27 @@ extern "C" {
 
 
 #ifdef GRIN_ENABLE_VERTEX_PRIMARY_KEYS
-/**
- * @brief get the vertex types with primary keys
- * @param GRIN_GRAPH the graph
-*/
 GRIN_VERTEX_TYPE_LIST grin_get_vertex_types_with_primary_keys(GRIN_GRAPH);
 
-/**
- * @brief get the primary keys (property list) of a specific vertex type
- * @param GRIN_GRAPH the graph
- * @param GRIN_VERTEX_TYPE the vertex type
-*/
 GRIN_VERTEX_PROPERTY_LIST grin_get_primary_keys_by_vertex_type(GRIN_GRAPH, GRIN_VERTEX_TYPE);
 
-/**
- * @brief get the vertex with the given primary keys
- * @param GRIN_GRAPH the graph
- * @param GRIN_VERTEX_TYPE the vertex type which determines the property list for primary keys
- * @param GRIN_ROW the values of primary keys
-*/
-GRIN_VERTEX grin_get_vertex_by_primary_keys(GRIN_GRAPH, GRIN_VERTEX_TYPE, GRIN_ROW);
+GRIN_ROW grin_get_primary_keys_row_by_vertex(GRIN_GRAPH, GRIN_VERTEX);
 #endif
 
-#ifdef GRIN_WITH_EDGE_PRIMARY_KEYS
-/**
- * @brief get the edge types with primary keys
- * @param GRIN_GRAPH the graph
-*/
+#ifdef GRIN_ENABLE_VERTEX_PK_OF_INT64
+long long int grin_get_vertex_pk_of_int64(GRIN_GRAPH, GRIN_VERTEX);
+#endif
+
+#ifdef GRIN_ENABLE_EDGE_PRIMARY_KEYS
 GRIN_EDGE_TYPE_LIST grin_get_edge_types_with_primary_keys(GRIN_GRAPH);
 
-/**
- * @brief get the primary keys (property list) of a specific edge type
- * @param GRIN_GRAPH the graph
- * @param GRIN_EDGE_TYPE the edge type
-*/
 GRIN_EDGE_PROPERTY_LIST grin_get_primary_keys_by_edge_type(GRIN_GRAPH, GRIN_EDGE_TYPE);
 
-/**
- * @brief get the edge with the given primary keys
- * @param GRIN_GRAPH the graph
- * @param GRIN_EDGE_PROPERTY_LIST the primary keys
- * @param GRIN_ROW the values of primary keys
-*/
-GRIN_EDGE grin_get_edge_by_primary_keys(GRIN_GRAPH, GRIN_EDGE_TYPE, GRIN_ROW);
+GRIN_ROW grin_get_primary_keys_row_by_edge(GRIN_GRAPH, GRIN_EDGE);
+#endif
+
+#ifdef GRIN_ENABLE_EDGE_PK_OF_INT64
+long long int grin_get_edge_pk_of_int64(GRIN_GRAPH, GRIN_EDGE);
 #endif
 
 #endif  // GRIN_INCLUDE_PROPERTY_PRIMARY_KEY_H_
