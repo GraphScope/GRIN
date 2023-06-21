814e1818e305a621e8c56afbeabbe11a3bdfe38c
diwen.zdw <diwen.zdw@alibaba-inc.com>
2023-06-21 12:31:19 +0800

    fix include

--- extension/src/list_chain.cc
+++ extension/src/list_chain.cc
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include "predefine.h"
 #include "list_chain.h"
 #include "property/topology.h"
 #include "property/type.h"
