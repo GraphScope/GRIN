5fd9bfd0e2b8ba38ad72f170d8a17db8c6dbf229
diwen.zdw <diwen.zdw@alibaba-inc.com>
2023-06-21 11:28:47 +0800

    update include of listchain

--- extension/src/list_chain.cc
+++ extension/src/list_chain.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "../include/list_chain.h"
+#include "list_chain.h"
 #include "property/topology.h"
 #include "property/type.h"
 #include "topology/vertexlist.h"
