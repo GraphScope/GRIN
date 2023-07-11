b7005c5ffa441394223c341fca0fa94abe42feef
diwen.zdw <diwen.zdw@alibaba-inc.com>
2023-07-11 16:52:31 +0800

    update label api based on grin-schema design

--- include/index/label.h
+++ include/index/label.h
@@ -22,83 +22,22 @@ extern "C" {
 #ifndef GRIN_INCLUDE_INDEX_LABEL_H_
 #define GRIN_INCLUDE_INDEX_LABEL_H_
 
-
-#if defined(GRIN_WITH_VERTEX_LABEL) || defined(GRIN_WITH_EDGE_LABEL)
-GRIN_LABEL grin_get_label_by_name(GRIN_GRAPH, const char*);
-
-void grin_destroy_label(GRIN_GRAPH, GRIN_LABEL);
-
-const char* grin_get_label_name(GRIN_GRAPH, GRIN_LABEL);
-
-void grin_destroy_label_list(GRIN_GRAPH, GRIN_LABEL_LIST);
-
-size_t grin_get_label_list_size(GRIN_GRAPH, GRIN_LABEL_LIST);
-
-GRIN_LABEL grin_get_label_from_list(GRIN_GRAPH, GRIN_LABEL_LIST, size_t);
-#endif
-
 #ifdef GRIN_WITH_VERTEX_LABEL
-/**
- * @brief assign a label to a vertex
- * @param GRIN_GRAPH the graph
- * @param GRIN_LABEL the label
- * @param GRIN_VERTEX the vertex
- * @return whether succeed
-*/
-bool grin_assign_label_to_vertex(GRIN_GRAPH, GRIN_LABEL, GRIN_VERTEX);
-
-/**
- * @brief get the label list of a vertex
- * @param GRIN_GRAPH the graph
- * @param GRIN_VERTEX the vertex
-*/
-GRIN_LABEL_LIST grin_get_vertex_label_list(GRIN_GRAPH, GRIN_VERTEX);
-
 /**
  * @brief get the vertex list by label
  * @param GRIN_GRAPH the graph
- * @param GRIN_LABEL the label
+ * @param label_name the label name
 */
-GRIN_VERTEX_LIST grin_get_vertex_list_by_label(GRIN_GRAPH, GRIN_LABEL);
-
-/**
- * @brief filtering an existing vertex list by label
- * @param GRIN_VERTEX_LIST the existing vertex list
- * @param GRIN_LABEL the label
-*/
-GRIN_VERTEX_LIST grin_select_label_for_vertex_list(GRIN_GRAPH, GRIN_LABEL, GRIN_VERTEX_LIST);
+GRIN_VERTEX_TYPE_LIST grin_get_vertex_types_by_label(GRIN_GRAPH, const char*);
 #endif
 
 #ifdef GRIN_WITH_EDGE_LABEL
 /**
- * @brief assign a label to a edge
- * @param GRIN_GRAPH the graph
- * @param GRIN_LABEL the label
- * @param GRIN_EDGE the edge
- * @return whether succeed
-*/
-bool grin_assign_label_to_edge(GRIN_GRAPH, GRIN_LABEL, GRIN_EDGE);
-
-/**
- * @brief get the label list of a edge
- * @param GRIN_GRAPH the graph
- * @param GRIN_EDGE the edge
-*/
-GRIN_LABEL_LIST grin_get_edge_label_list(GRIN_GRAPH, GRIN_EDGE);
-
-/**
  * @brief get the edge list by label
  * @param GRIN_GRAPH the graph
- * @param GRIN_LABEL the label
+ * @param label_name the label name
 */
-GRIN_EDGE_LIST grin_get_edge_list_by_label(GRIN_GRAPH, GRIN_LABEL);
-
-/**
- * @brief filtering an existing edge list by label
- * @param GRIN_EDGE_LIST the existing edge list
- * @param GRIN_LABEL the label
-*/
-GRIN_EDGE_LIST grin_select_label_for_edge_list(GRIN_GRAPH, GRIN_LABEL, GRIN_EDGE_LIST);
+GRIN_EDGE_TYPE_LIST grin_get_edge_types_by_label(GRIN_GRAPH, const char*);
 #endif
 
 #endif // GRIN_INCLUDE_INDEX_LABEL_H_
