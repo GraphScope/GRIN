71a9df49ad90671129d34c40bd42d3de1d955a19
Lei Wang <jiede.wl@alibaba-inc.com>
2023-08-08 15:26:28 +0800

    Add test for GRIN extension
    Signed-off-by: Lei Wang <jiede.wl@alibaba-inc.com>

--- extension/src/indexed_adjacent_list.cc
+++ extension/src/indexed_adjacent_list.cc
@@ -26,6 +26,17 @@ limitations under the License.
 
 #if defined (GRIN_ENABLE_ADJACENT_LIST_ITERATOR) && !defined(GRIN_ENABLE_ADJACENT_LIST_ARRAY)
 
+void _prepare_cache_for_indexed_adjacent_list(GRIN_GRAPH g, GRIN_INDEXED_ADJACENT_LIST indexed_adj_list) {
+    auto _indexed_adj_list = static_cast<GRIN_INDEXED_ADJACENT_LIST_T*>(indexed_adj_list);
+    while (!grin_is_adjacent_list_end(g, _indexed_adj_list->iterator)) {
+        auto dst_vertex = grin_get_neighbor_from_adjacent_list_iter(g, _indexed_adj_list->iterator);
+        _indexed_adj_list->neighbors.push_back(dst_vertex);
+        auto edge = grin_get_edge_from_adjacent_list_iter(g, _indexed_adj_list->iterator);
+        _indexed_adj_list->edges.push_back(edge);
+        grin_get_next_adjacent_list_iter(g, _indexed_adj_list->iterator);
+    }
+}
+
 GRIN_INDEXED_ADJACENT_LIST grin_get_indexed_adjacent_list(GRIN_GRAPH g, GRIN_ADJACENT_LIST adj_list) {
     GRIN_INDEXED_ADJACENT_LIST_T* indexed_adj_list = new GRIN_INDEXED_ADJACENT_LIST_T();
     indexed_adj_list->iterator = grin_get_adjacent_list_begin(g, adj_list);
@@ -52,17 +63,9 @@ size_t grin_get_indexed_adjacent_list_size(GRIN_GRAPH g, GRIN_INDEXED_ADJACENT_L
     if (_indexed_adj_list->cache_built) {
         return _indexed_adj_list->neighbors.size();
     }
-    size_t result = 0;
-    while (!grin_is_adjacent_list_end(g, _indexed_adj_list->iterator)) {
-        result++;
-        auto dst_vertex = grin_get_neighbor_from_adjacent_list_iter(g, _indexed_adj_list->iterator);
-        _indexed_adj_list->neighbors.push_back(dst_vertex);
-        auto edge = grin_get_edge_from_adjacent_list_iter(g, _indexed_adj_list->iterator);
-        _indexed_adj_list->edges.push_back(edge);
-        grin_get_next_adjacent_list_iter(g, _indexed_adj_list->iterator);
-    }
+    _prepare_cache_for_indexed_adjacent_list(g, indexed_adj_list);
     _indexed_adj_list->cache_built = true;
-    return result;
+    return _indexed_adj_list->neighbors.size();
 }
 
 GRIN_VERTEX grin_get_neighbor_from_indexed_adjacent_list(GRIN_GRAPH g, GRIN_INDEXED_ADJACENT_LIST indexed_adj_list, size_t index) {
@@ -73,14 +76,8 @@ GRIN_VERTEX grin_get_neighbor_from_indexed_adjacent_list(GRIN_GRAPH g, GRIN_INDE
         }
         return _indexed_adj_list->neighbors[index];
     }
+    _prepare_cache_for_indexed_adjacent_list(g, indexed_adj_list);
     _indexed_adj_list->cache_built = true;
-    while (!grin_is_adjacent_list_end(g, _indexed_adj_list->iterator)) {
-        auto dst_vertex = grin_get_neighbor_from_adjacent_list_iter(g, _indexed_adj_list->iterator);
-        _indexed_adj_list->neighbors.push_back(dst_vertex);
-        auto edge = grin_get_edge_from_adjacent_list_iter(g, _indexed_adj_list->iterator);
-        _indexed_adj_list->edges.push_back(edge);
-        grin_get_next_adjacent_list_iter(g, _indexed_adj_list->iterator);
-    }
     if (index >= _indexed_adj_list->neighbors.size()) {
         return GRIN_NULL_VERTEX;
     }
@@ -96,13 +93,7 @@ GRIN_EDGE grin_get_edge_from_indexed_adjacent_list(GRIN_GRAPH g, GRIN_INDEXED_AD
         return _indexed_adj_list->edges[index];
     }
     _indexed_adj_list->cache_built = true;
-    while (!grin_is_adjacent_list_end(g, _indexed_adj_list->iterator)) {
-        auto dst_vertex = grin_get_neighbor_from_adjacent_list_iter(g, _indexed_adj_list->iterator);
-        _indexed_adj_list->neighbors.push_back(dst_vertex);
-        auto edge = grin_get_edge_from_adjacent_list_iter(g, _indexed_adj_list->iterator);
-        _indexed_adj_list->edges.push_back(edge);
-        grin_get_next_adjacent_list_iter(g, _indexed_adj_list->iterator);
-    }
+    _prepare_cache_for_indexed_adjacent_list(g, indexed_adj_list);
     if (index >= _indexed_adj_list->edges.size()) {
         return GRIN_NULL_EDGE;
     }
