77fd74e397d7583897030754c97944fdce36312c
diwen.zdw <diwen.zdw@alibaba-inc.com>
2023-08-15 09:19:47 +0800

    fix ext_test bugs

--- test/ext_test.c
+++ test/ext_test.c
@@ -3,9 +3,14 @@
 #include "predefine.h"
 #include "partition/partition.h"
 #include "topology/structure.h"
+#include "topology/vertexlist.h"
+#include "topology/adjacentlist.h"
 #include "property/type.h"
+#include "property/topology.h"
+#include "partition/topology.h"
 #include "index/internal_id.h"
 #include "list_chain.h"
+#include "indexed_adjacent_list.h"
 
 GRIN_GRAPH get_graph(const char* uri, int p) {
 #ifdef GRIN_ENABLE_GRAPH_PARTITION
@@ -96,27 +101,34 @@ void test_adjacent_list_chain(const char* uri, GRIN_DIRECTION d) {
   grin_destroy_graph(g);
 }
 
-void test_indexed_adjacent_list(const char* uri) {
+#if defined (GRIN_ENABLE_ADJACENT_LIST_ITERATOR) && !defined(GRIN_ENABLE_ADJACENT_LIST_ARRAY)
+void test_indexed_adjacent_list(const char* uri, GRIN_DIRECTION d) {
   GRIN_GRAPH g = get_graph(uri, 0);
-  auto vertex_list = grin_get_vertex_list_by_type(g, 0);
-  auto vertex_iter = grin_get_vertex_list_begin(g, vertex_list);
+  GRIN_VERTEX_LIST vertex_list = grin_get_vertex_list_by_type_select_master(g, 0);
+  GRIN_VERTEX_LIST_ITERATOR vertex_iter = grin_get_vertex_list_begin(g, vertex_list);
   while (!grin_is_vertex_list_end(g, vertex_iter)) {
-    auto v = grin_get_vertex_from_iter(g, vertex_iter);
+    GRIN_VERTEX v = grin_get_vertex_from_iter(g, vertex_iter);
     GRIN_EDGE_TYPE_LIST etl = grin_get_edge_type_list(g);
     size_t etl_size = grin_get_edge_type_list_size(g, etl);
     for (size_t etl_i = 0; etl_i < etl_size; ++etl_i) {
-      auto adj_list = grin_get_adjacent_list_by_edge_type(g, GRIN_DIRECTION::OUT, v, etl_i);
-      auto indexed_adj_list = grin_get_indexed_adjacent_list(g, adj_list);
+      GRIN_ADJACENT_LIST adj_list = grin_get_adjacent_list_by_edge_type(g, d, v, etl_i);
+      GRIN_INDEXED_ADJACENT_LIST indexed_adj_list = grin_get_indexed_adjacent_list(g, adj_list);
       size_t indexed_adj_list_size = grin_get_indexed_adjacent_list_size(g, indexed_adj_list);
-      for (auto idx = 0; idx < indexed_adj_list_size; ++idx) {
-        auto dst_vertex = grin_get_neighbor_from_indexed_adjacent_list(g, indexed_adj_list, idx);
-        auto edge = grin_get_edge_from_indexed_adjacent_list(g, indexed_adj_list, idx);
+      for (size_t idx = 0; idx < indexed_adj_list_size; ++idx) {
+        GRIN_VERTEX dst_vertex = grin_get_neighbor_from_indexed_adjacent_list(g, indexed_adj_list, idx);
+        GRIN_EDGE edge = grin_get_edge_from_indexed_adjacent_list(g, indexed_adj_list, idx);
         GRIN_VERTEX_TYPE dst_vt = grin_get_vertex_type(g, dst_vertex);
       #ifdef GRIN_ENABLE_VERTEX_INTERNAL_ID_INDEX
-        long long int v_id = grin_get_vertex_internal_id_by_type(g, dst_vt, dst_vertex);
-        GRIN_EDGE_TYPE et = grin_get_edge_type(g, e);
-        printf("%s %lld %s\n", grin_get_vertex_type_name(g, dst_vertex), v_id,
-               grin_get_edge_type_name(g, et));
+        long long int src_v_id = grin_get_vertex_internal_id_by_type(g, 0, v);
+        long long int dst_v_id = grin_get_vertex_internal_id_by_type(g, dst_vt, dst_vertex);
+        GRIN_EDGE_TYPE et = grin_get_edge_type(g, edge);
+        if (d == OUT) {
+          printf("OUT %s %lld %s %s %lld\n", grin_get_vertex_type_name(g, 0), src_v_id,
+                grin_get_edge_type_name(g, et), grin_get_vertex_type_name(g, dst_vt), dst_v_id);
+        } else {
+          printf("IN %s %lld %s %s %lld\n", grin_get_vertex_type_name(g, dst_vt), dst_v_id,
+                grin_get_edge_type_name(g, et), grin_get_vertex_type_name(g, 0), src_v_id);
+        }
       #endif
         grin_destroy_vertex(g, dst_vertex);
         grin_destroy_edge(g, edge);
@@ -130,6 +142,7 @@ void test_indexed_adjacent_list(const char* uri) {
   }
   grin_destroy_graph(g);
 }
+#endif
 
 int main(int argc, char** argv) {
 #if defined(GRIN_WITH_VERTEX_PROPERTY) && defined(GRIN_WITH_EDGE_PROPERTY) && \
@@ -139,7 +152,8 @@ int main(int argc, char** argv) {
   test_adjacent_list_chain(argv[1], IN);
 #endif
 #if defined (GRIN_ENABLE_ADJACENT_LIST_ITERATOR) && !defined(GRIN_ENABLE_ADJACENT_LIST_ARRAY)
-  test_indexed_adjacent_list(argv[1])
+  test_indexed_adjacent_list(argv[1], OUT);
+  test_indexed_adjacent_list(argv[1], IN);
 #endif
   return 0;
 }
