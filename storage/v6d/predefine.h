6cf4984e85ff1f17bd5e8558275aebeabe658915
diwen.zdw <diwen.zdw@alibaba-inc.com>
2023-11-06 19:37:41 +0800

    quick fix

--- storage/v6d/predefine.h
+++ storage/v6d/predefine.h
@@ -67,7 +67,7 @@ typedef unsigned long long int GRIN_VERTEX;
 typedef struct GRIN_EDGE {
   GRIN_VERTEX src;
   GRIN_VERTEX dst;
-  GRIN_DIRECTION dir;
+  int dir;
   unsigned etype;
   unsigned long long int eid;
 } GRIN_EDGE;                 
@@ -89,7 +89,7 @@ typedef struct GRIN_ADJACENT_LIST {
   const void* begin;
   const void* end;
   GRIN_VERTEX vid;
-  GRIN_DIRECTION dir;
+  int dir;
   unsigned etype;
 } GRIN_ADJACENT_LIST;           
 #endif
