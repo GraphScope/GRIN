ea5c6e768756608062a43c836c9bd565e86b7d4f
有理 <youli.lx@alibaba-inc.com>
2024-02-26 15:22:28 +0800

    topology

--- demo_storage/src/topology/structure.cc
+++ demo_storage/src/topology/structure.cc
@@ -49,11 +49,9 @@ bool grin_is_directed(GRIN_GRAPH g) {
 bool grin_is_multigraph(GRIN_GRAPH) { return true; }
 #endif
 
-#if !defined(GRIN_WITH_VERTEX_PROPERTY)
+#ifndef GRIN_ENABLE_SCHEMA
 size_t grin_get_vertex_num(GRIN_GRAPH);
-#endif
 
-#if !defined(GRIN_WITH_EDGE_PROPERTY)
 size_t grin_get_edge_num(GRIN_GRAPH);
 #endif
 
